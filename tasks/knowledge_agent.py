de188752a972dbd8e8c46adf0af8646cd71efb65
neojingyi <neojingyi7@gmail.com>
2025-04-11 02:11:07 +0800

    updated tasks and tools

---
+++ tasks/knowledge_agent.py
@@ -0,0 +1,9 @@
+from crewai import Agent
+
+KnowledgeAgent = Agent(
+    role='Information Specialist',
+    goal='Answer questions accurately using real-time knowledge from Valyu.',
+    backstory='You are an intelligent assistant who knows how to query the Valyu API.'
+    allow_delegation=False,
+    verbose=True
+)
---
+++ tools/valyu_tool.py
@@ -0,0 +1,24 @@
+import requests
+import os
+
+class ValyuTool:
+    def __init__(self):
+        self.api_key=os.getenv("VALYU_API_KEY")
+
+    def fetch_knowledge(self,query):
+        url="https://api.valyu.network/v1/knowledge"
+        headers = {
+            "x-api-key": "self.api_key",
+            "Content-Type": "application/json"
+            }
+        payload = {
+            "query": "<string>",
+            "search_type": "proprietary",
+            "data_sources": ["valyu/valyu-arxiv", "valyu/valyu-wikipedia", "https://www.valyu.network/"],
+            "max_num_results": 10,
+            "similarity_threshold": 0.4,
+            "query_rewrite": True,
+            "max_price": 123
+        }
+        response = requests.request("POST", url, json=payload, headers=headers)
+        print(response.text)
